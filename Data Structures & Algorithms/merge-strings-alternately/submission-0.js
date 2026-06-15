3309cf9118ca98f91c560ab2cef05d2625bb1515
Harshita <76911212+harshita2412@users.noreply.github.com>
2026-06-16 00:28:42 +0530

    Add: merge-strings-alternately - submission-0

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-0.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {string} word1
+     * @param {string} word2
+     * @return {string}
+     */
+    mergeAlternately(word1, word2) {
+        let result=[];
+        let i=0;
+
+        while(i< word1.length || i<word2.length){
+            result.push(word1[i], word2[i])
+            i++;
+        }
+        return result.join('');
+    }
+}
