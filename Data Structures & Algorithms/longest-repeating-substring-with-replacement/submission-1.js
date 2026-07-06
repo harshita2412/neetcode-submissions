f37d73c3d3212dcccc1553dea2669da4225c8ab7
Harshita <76911212+harshita2412@users.noreply.github.com>
2026-07-06 21:43:44 +0530

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        //sliding window
+        const count = new Map();
+        let maxFreq = 0, result =0
+        let left = 0;
+        for(let right =0; right<s.length; right++){
+            count.set(s[right], (count.get(s[right])|| 0)+1)
+            maxFreq = Math.max(maxFreq, count.get(s[right]));
+
+            if((right - left +1) - maxFreq> k){
+                count.set(s[left], count.get(s[left])-1 );
+                left++;
+            }
+            result = Math.max(result, right - left +1)
+        }
+        return result
+    }
+}
